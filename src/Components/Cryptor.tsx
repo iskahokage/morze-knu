303d1d14198c108242b6da29f64b8f6d591eeaf5
Iskender <iskndrsmt@gmail.com>
2024-03-07 20:03:45 +0600

    fix

--- src/Components/Cryptor.tsx
+++ src/Components/Cryptor.tsx
@@ -3,51 +3,59 @@ import { morseCode } from "../helper";
 import { FunctionBody } from "typescript";
 
 interface cryptorProps {
-    trigger: () => void;
+  trigger: () => void;
 }
 
 const Cryptor: React.FC<cryptorProps> = ({ trigger }) => {
-    type EventHandler = (e: React.ChangeEvent<HTMLInputElement>) => string;
+  type EventHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => string;
 
-    const [text, setText] = useState("");
-    const [crypted, setCrypted] = useState("");
+  const [text, setText] = useState("");
+  const [crypted, setCrypted] = useState("");
 
-    const textHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        const text: string = e.target.value;
-        setText(text);
-    };
+  const textHandler = (
+    e: ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const text: string = e.target.value;
+    setText(text);
+  };
 
-    const encodeToMorse = () => {
-        // const replacedSpace = text.replace(/ /g, "|");
-        const cryptedResult = text
-            .toLowerCase() // Переводим текст в нижний регистр
-            .split("") // Разбиваем текст на массив символов
-            .map((char: string) => morseCode[char] || char) // Заменяем символы на их эквивалент в азбуке Морзе, если он существует
-            .join(" "); // Соединяем символы обратно в строку с пробелами между ними
-        setCrypted(cryptedResult);
-        trigger();
-        localStorage.setItem("crypted", JSON.stringify(cryptedResult));
-    };
-
-    return (
-        <div className="container mx-auto bg-gray-200 rounded-lg p-5 grid gap-4">
-            <h2 className="text-lg">Напишите текст для шифровки в морзе:</h2>
-            <textarea
-                onChange={textHandler}
-                className="bg-transparent p-2 border border-gray-500 rounded-lg  outline-none"
-            />
-            <button
-                onClick={encodeToMorse}
-                className="bg-gray-700 rounded-lg p-3 text-gray-50 hover:bg-gray-500 active:bg:gray-700 focus:bg-gray-600 outline-none"
-            >
-                Зашифровать
-            </button>
-            <h2 className="text-lg">Результат шифрования:</h2>
-            <div className="bg-gray-100 p-5 rounded-lg">
-                <p className="text-center">{crypted}</p>
-            </div>
-        </div>
+  const encodeToMorse = () => {
+    const cryptedResult = text
+      .toLowerCase() // Переводим текст в нижний регистр
+      .split("") // Разбиваем текст на массив символов
+      .map((char: string) => morseCode[char] || char) // Заменяем символы на их эквивалент в азбуке Морзе, если он существует
+      .join(" "); // Соединяем символы обратно в строку с пробелами между ними
+    setCrypted(cryptedResult);
+    localStorage.setItem(
+      "crypted",
+      JSON.stringify(cryptedResult)
     );
+    trigger(); //Триггерим компоненту дешифратора, что бы она поняла, что у нас localStorage не пустой
+  };
+
+  return (
+    <div className="container mx-auto bg-gray-200 rounded-lg p-5 grid gap-4">
+      <h2 className="text-lg">
+        Напишите текст для шифровки в морзе:
+      </h2>
+      <textarea
+        onChange={textHandler}
+        className="bg-transparent p-2 border border-gray-500 rounded-lg  outline-none"
+      />
+      <button
+        onClick={encodeToMorse}
+        className="bg-gray-700 rounded-lg p-3 text-gray-50 hover:bg-gray-500 active:bg:gray-700 focus:bg-gray-600 outline-none"
+      >
+        Зашифровать
+      </button>
+      <h2 className="text-lg">Результат шифрования:</h2>
+      <div className="bg-gray-100 p-5 rounded-lg">
+        <p className="text-center">{crypted}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Cryptor;
--- src/Components/Decryptor.tsx
+++ src/Components/Decryptor.tsx
@@ -1,81 +1,96 @@
-import React, { ChangeEvent, FC, useEffect, useRef, useState } from "react";
+import React, {
+  ChangeEvent,
+  FC,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { morseCode } from "../helper";
 
 interface decryptorProps {
-    trigger: boolean;
+  trigger: boolean;
 }
 
 const Decryptor: FC<decryptorProps> = ({ trigger }) => {
-    const [cryptedData, setCryptedData] = useState("");
-    const [text, setText] = useState("");
-    const [decryptedData, setDecryptedData] = useState("");
+  const [cryptedData, setCryptedData] = useState("");
+  const [text, setText] = useState("");
+  const [decryptedData, setDecryptedData] = useState("");
 
-    useEffect(() => {
-        const crypted = localStorage.getItem("crypted");
-        setCryptedData(!!crypted ? JSON.parse(crypted) : "");
-    }, [trigger]);
+  useEffect(() => {
+    const crypted = localStorage.getItem("crypted");
+    setCryptedData(!!crypted ? JSON.parse(crypted) : "");
+  }, [trigger]);
 
-    const ref = useRef<HTMLTextAreaElement>(null);
+  const ref = useRef<HTMLTextAreaElement>(null);
 
-    const pasteData = () => {
-        if (ref.current) {
-            ref.current.value = cryptedData;
-            setText(cryptedData);
-        }
-    };
+  const pasteData = () => {
+    if (ref.current) {
+      ref.current.value = cryptedData;
+      setText(cryptedData);
+    }
+  };
 
-    const textHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        const a: string = e.target.value;
-        console.log(a);
-        setText(a);
-    };
+  const textHandler = (
+    e: ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const a: string = e.target.value;
+    setText(a);
+  };
 
-    const decodeFromMorse = () => {
-        const morseMap: { [key: string]: string } = Object.fromEntries(
-            Object.entries(morseCode).map(([key, value]) => [value, key])
-        );
-        const decryptedResult = text
-            .split(" ") // Разбиваем код Морзе на отдельные символы
-            .map((char: string) => morseMap[char] || char) // Заменяем символы на их эквиваленты в алфавите, если они существуют
-            .join(""); // Соединяем буквы
-        setDecryptedData(decryptedResult);
-    };
+  const decodeFromMorse = () => {
+    const morseMap: { [key: string]: string } =
+      Object.fromEntries(
+        Object.entries(morseCode).map(([key, value]) => [
+          value,
+          key,
+        ])
+      );
+    const decryptedResult = text
+      .split(" ") // Разбиваем код Морзе на отдельные символы
+      .map((char: string) => morseMap[char] || char) // Заменяем символы на их эквиваленты в алфавите, если они существуют
+      .join(""); // Соединяем буквы
+    setDecryptedData(decryptedResult);
+  };
 
-    return (
-        <div className="container mx-auto bg-gray-200 rounded-lg p-5 grid gap-4">
-            <h1 className="text-lg">
-                Напишите текст для дешифровки из морзе в текст:
-            </h1>
-            <textarea
-                ref={ref}
-                onChange={textHandler}
-                className="bg-transparent p-2 border border-gray-500 rounded-lg  outline-none"
-            />
-            <div className="grid gap-4">
-                <div className="grid gap-4 grid-cols-2">
-                    <button
-                        onClick={pasteData}
-                        disabled={!cryptedData}
-                        className="bg-gray-700 rounded-lg p-3 text-gray-50 hover:bg-gray-500 active:bg:gray-700 focus:bg-gray-600 disabled:bg-gray-400 outline-none"
-                    >
-                        Вставить результат шифрования
-                    </button>
-                    <button
-                        onClick={decodeFromMorse}
-                        className="bg-gray-700 rounded-lg p-3 text-gray-50 hover:bg-gray-500 active:bg:gray-700 focus:bg-gray-600 outline-none"
-                    >
-                        Расшифровать
-                    </button>
-                </div>
-                <div className="grid gap-4">
-                    <h2 className="text-lg">Результат дешифрования:</h2>
-                    <div className="bg-gray-100 p-5 rounded-lg">
-                        <p className="text-center">{decryptedData}</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container mx-auto bg-gray-200 rounded-lg p-5 grid gap-4">
+      <h1 className="text-lg">
+        Напишите текст для дешифровки из морзе в текст:
+      </h1>
+      <textarea
+        ref={ref}
+        onChange={textHandler}
+        className="bg-transparent p-2 border border-gray-500 rounded-lg  outline-none"
+      />
+      <div className="grid gap-4">
+        <div className="grid gap-4 grid-cols-2">
+          <button
+            onClick={pasteData}
+            disabled={!cryptedData}
+            className="bg-gray-700 rounded-lg p-3 text-gray-50 hover:bg-gray-500 
+            active:bg:gray-700 focus:bg-gray-600 disabled:bg-gray-400 outline-none"
+          >
+            Вставить результат шифрования
+          </button>
+          <button
+            onClick={decodeFromMorse}
+            className="bg-gray-700 rounded-lg p-3 text-gray-50 hover:bg-gray-500
+            active:bg:gray-700 focus:bg-gray-600 outline-none"
+          >
+            Расшифровать
+          </button>
         </div>
-    );
+        <div className="grid gap-4">
+          <h2 className="text-lg">
+            Результат дешифрования:
+          </h2>
+          <div className="bg-gray-100 p-5 rounded-lg">
+            <p className="text-center">{decryptedData}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Decryptor;
